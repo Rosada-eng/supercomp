8bb56095726a3b785ec94732718f4ab1eaeb5ce4
Guilherme Rosada <guilhermer4@al.insper.edu.br>
2023-03-31 00:43:32 -0300

    synthesize function is_available

--- material/aulas/maratona_filmes/heuristica.cpp
+++ material/aulas/maratona_filmes/heuristica.cpp
@@ -45,6 +45,44 @@ void book_a_movie(vector<movie> &book, vector<int> &movies_booked, vector<int> &
     categories_booked.push_back(book[movie_id].category);
 }
 
+bool is_movie_available(vector<int> &movies_booked, vector<int> &categories_booked, vector<int> &categories_max_size, vector<movie> &book, int movie_id, int last_seen_finishes_at) {
+    // checa se o filme já não foi assistido
+    bool movie_is_taken = find(movies_booked.begin(), movies_booked.end(), movie_id) != movies_booked.end();
+
+    // checa se estourou limite para categoria
+    bool category_limit_exceeded = count(categories_booked.begin(), categories_booked.end(), book[movie_id].category) >= categories_max_size[book[movie_id].category];
+
+    // checa se o filme anterior já acabou no horário de início do filme atual
+    bool is_time_available = (movies_booked.size() == 0) || (book[movie_id].start >= last_seen_finishes_at);
+
+    if (DEBUG) {
+        cout << "M" << movie_id << "\t" << book[movie_id].start << "~" << book[movie_id].end << " C" << book[movie_id].category;
+
+        if (!is_time_available) {
+            cout << "\t time is not available "
+                 << "(" << book[movies_booked.back()].end << ")";
+        }
+        if (category_limit_exceeded) {
+            cout << "\t category limit exceeded";
+        }
+        if (movie_is_taken) {
+            cout << "\t movie is taken";
+        }
+        if (!category_limit_exceeded && !movie_is_taken && is_time_available) {
+            cout << "\t is available";
+        }
+        cout << endl;
+    }
+
+    // passa em todos os filtros
+    if (!category_limit_exceeded && !movie_is_taken && is_time_available) {
+
+        return true;
+    }
+
+    return false;
+}
+
 int main() {
     vector<movie> book;
     int n = 0;
@@ -84,48 +122,16 @@ int main() {
 
     sort_book_by_ending_time(book, sorting_movies);
 
-    vector<int> booked_backup;
     int last_seen_finishes_at = 0;
     for (int day = 0; day < 2; day++) {
         for (int i = 0; i < n; i++) {
             int movie_id = sorting_movies[i];
-            int category = book[movie_id].category;
 
-            // checa se o filme já não foi assistido
-            bool movie_is_taken = find(movies_booked.begin(), movies_booked.end(), movie_id) != movies_booked.end();
+            bool is_available = is_movie_available(movies_booked, categories_booked, categories_max_size, book, movie_id, last_seen_finishes_at);
 
-            // checa se estourou limite para categoria
-            bool category_limit_exceeded = count(categories_booked.begin(), categories_booked.end(), category) >= categories_max_size[category];
-
-            // checa se o filme anterior já acabou no horário de início do filme atual
-            bool is_time_available = (movies_booked.size() == 0) || (book[movie_id].start >= last_seen_finishes_at);
-
-            if (DEBUG) {
-                cout << "M" << movie_id << "\t" << book[movie_id].start << "~" << book[movie_id].end << " C" << book[movie_id].category;
-            }
-
-            // se passar nos dois filtros, adiciona o filme
-            if (!category_limit_exceeded && !movie_is_taken && is_time_available) {
-                if (DEBUG) {
-                    cout << "\t is available";
-                }
+            if (is_available) {
                 book_a_movie(book, movies_booked, categories_booked, movie_id);
-
                 last_seen_finishes_at = book[movie_id].end;
-            }
-
-            if (DEBUG) {
-                if (!is_time_available) {
-                    cout << "\t time is not available "
-                         << "(" << book[movies_booked.back()].end << ")";
-                }
-                if (category_limit_exceeded) {
-                    cout << "\t category limit exceeded";
-                }
-                if (movie_is_taken) {
-                    cout << "\t movie is taken";
-                }
-                cout << endl;
             }
         }
 
